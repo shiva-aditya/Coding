c8d9ee539af17bce83224c0983da578b852b5c61
Shiva K <21a95a0208@aec.edu.in>
2023-01-23 22:47:54 +0530

    Create Stack STL.cpp

---
+++ Stack STL.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    stack<int>st;
+    st.empty();
+    //gives true if empty else false
+    st.size();
+    //gives size of stack (no of elements)
+    st.top();
+    //gives element at top of stack
+    st.push(1);
+    //push element to top of stack
+    st.pop();
+    //pop the top element
+}
